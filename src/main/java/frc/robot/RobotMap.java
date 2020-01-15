3341555098e34ae40ec29431e26ad78334506808
mfitzell <mfitzell@comcast.net>
2020-01-14 20:28:15 -0500

    RobotMap updates, now featuring comments

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -11,15 +11,10 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
+/** Used to map hardware ports and define hardware objects - motor controllers, joysticks, cameras, etc */
 public final class RobotMap {
 
-
+  /** Motor controllers for the drive train */
   public static class DrivetrainControllers {
     public static final int DRIVE_TRAIN_RIGHT_MASTER = 8;
     public static final int DRIVE_TRAIN_RIGHT_SLAVE1 = 7;
@@ -38,6 +33,7 @@ public final class RobotMap {
     //public static VictorSPX RightFollower2 = new VictorSPX(DRIVE_TRAIN_RIGHT_SLAVE2);
   }
 
+  /** Integer settings for the vision subsystem - change camera mode, pipeline, and LED state using these */
   public static class LimelightSettings {
     public static int visionMode = 0;
     public static int cameraMode = 1;
@@ -46,6 +42,7 @@ public final class RobotMap {
     public static int ledsOff = 1;
   }
 
+  /** Driver joysticks and operator controllers */
   public static class Joysticks {
     public static int leftStickPort = 3;
     public static int rightStickPort = 2;
@@ -54,9 +51,9 @@ public final class RobotMap {
     public static Joystick driverStickLeft = new Joystick(leftStickPort);
     public static Joystick driverStickRight = new Joystick(rightStickPort);
     public static Joystick operatorController = new Joystick(controllerPort);
-
   }
 
+  /** Button mapping, for joysticks and controllers */
   public static class Buttons {
     public static int visionTrack = 1;
   }
