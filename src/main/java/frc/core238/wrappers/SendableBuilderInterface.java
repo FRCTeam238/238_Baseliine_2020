43fc299101fe739ca5424d8dd0e33d3bbd4c6c2f
Robert Durell <rdurell@gmail.com>
2020-01-17 14:05:46 -0500

    added sendable class for arbitrary sendables

---
+++ src/main/java/frc/core238/wrappers/SendableBuilderInterface.java
@@ -0,0 +1,7 @@
+package frc.core238.wrappers;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+
+public interface SendableBuilderInterface {
+    public void initSendable(SendableBuilder builder);
+}
