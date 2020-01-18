c4b07592783a361e211c2003d200ad637bb5687a
mfitzell <mfitzell@comcast.net>
2020-01-18 16:21:55 -0500

    added game piece constants

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -3,9 +3,26 @@ package frc.robot;
 /** Class for mathematic functions */
 public class FieldConstants {
 
-    /** Heights of vision camera and targets, in inches */
+    /** Heights of vision camera and targets, ien inches */
     public static class VisionConstants {
         static final double targetHeight = 37;
         static final double cameraHeight = 25;
+
+        public static double getTargetheight() {
+            return targetHeight;
+        }
+
+        public static double getCameraheight() {
+            return cameraHeight;
+        }
     }
+
+    public static class GamePieces {
+        static final double ballRadius = 3.5; //in inches
+
+        public static double getBallradius() {
+            return ballRadius;
+        }
+    }
+
 }
