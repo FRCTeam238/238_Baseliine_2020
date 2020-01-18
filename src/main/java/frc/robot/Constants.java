4acdd4cd55429319b98e4cdced918c058cfdf2bd
mfitzell <mfitzell@comcast.net>
2020-01-17 19:54:52 -0500

    TalonSRX PID initial wrapper work

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/**
+ * Class containing numbers used throughout the project - PID values, as well as mathematic constants
+ */
+public final class Constants {
+
+  public static class CTRE_PID {
+    public static double kP = 0;
+    public static double kI = 0;
+    public static double kD = 0;
+    public static double kF = 0;
+    public static int kIzone = 100;
+    public static int kPIDLoopIdx = 0;
+    public static int kTimeoutMs = 30;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/CTRE_PID.java
@@ -0,0 +1,77 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Constants;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class CTRE_PID extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  TalonSRX leftTalon = RobotMap.DrivetrainControllers.LeftMaster;
+  TalonSRX rightTalon = RobotMap.DrivetrainControllers.RightMaster;
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void prepTalons(){
+    /* Config the peak and nominal outputs ([-1, 1] represents [-100, 100]%) */
+    leftTalon.configNominalOutputForward(0, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.configNominalOutputReverse(0, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.configPeakOutputForward(1, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.configPeakOutputReverse(-1, Constants.CTRE_PID.kTimeoutMs);
+
+    rightTalon.configNominalOutputForward(0, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.configNominalOutputReverse(0, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.configPeakOutputForward(1, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.configPeakOutputReverse(-1, Constants.CTRE_PID.kTimeoutMs);
+
+    /**
+     * Config the allowable closed-loop error, Closed-Loop output will be
+     * neutral within this range.
+     */
+    leftTalon.configAllowableClosedloopError(0, Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.configAllowableClosedloopError(0, Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kTimeoutMs);
+
+    /* Config closed loop gains for Primary closed loop (Current) */
+    leftTalon.config_kP(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kP, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.config_kI(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kI, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.config_kD(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kD, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.config_kF(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kF, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.config_IntegralZone(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kIzone, Constants.CTRE_PID.kTimeoutMs);
+
+    rightTalon.config_kP(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kP, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.config_kI(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kI, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.config_kD(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kD, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.config_kF(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kF, Constants.CTRE_PID.kTimeoutMs);
+    rightTalon.config_IntegralZone(Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kIzone, Constants.CTRE_PID.kTimeoutMs);
+    
+    // Config encoders
+    rightTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kTimeoutMs);
+
+    // Ensure motor output and encoder velocity are proportional to each other
+    // If they become inverted, set these to true
+    rightTalon.setSensorPhase(false);
+    leftTalon.setSensorPhase(false);
+
+    // Zeroes encoders
+    rightTalon.setSelectedSensorPosition(0, Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kTimeoutMs);
+    leftTalon.setSelectedSensorPosition(0, Constants.CTRE_PID.kPIDLoopIdx, Constants.CTRE_PID.kTimeoutMs);
+  }
+}
